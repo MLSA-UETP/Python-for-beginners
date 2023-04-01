f4b167b508076d2ba64170cc266adc8e072d0d6a
Waliiya Rizwan <72751932+WaliiyaRizwan@users.noreply.github.com>
2023-04-01 17:44:39 +0000

    adding tuple

---
+++ module03/tuple.py
@@ -0,0 +1,17 @@
+#A tuple is defined using parentheses (()) and consists of a series of values, separated by commas
+my_tuple = (1, 2, 3, 4)
+
+
+#Tuples are another important data type in Python, which are similar to lists but are immutable, meaning they cannot be modified once they are created.
+
+#accessing tuples
+
+my_tuple[0]
+
+
+#You can also use tuples for unpacking, which is a way to assign the values in a tuple to separate variables
+my_tuple = (1, 2, 3)
+a, b, c = my_tuple
+
+
+
