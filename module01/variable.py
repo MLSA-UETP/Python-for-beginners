4965cf081a2f302b5a7160d1bd6d2b90da565ed9
Saqlain Ahmad <92431771+EngineerSaqlain@users.noreply.github.com>
2023-04-03 03:05:03 +0500

    Update variable.py

--- module01/variable.py
+++ module01/variable.py
@@ -4,6 +4,11 @@ print(product)
 
 print(sum)
 
-print(f"This is my sum: {sum}")  #f is used for formate identifier.
-                                #It is used to make the code more simpler and easy to understand
+print(f"This is my sum: {sum}")  
+
+#f is used for formate identifier.
+#These strings may contain replacement fields, which are expressions delimited by curly braces {}.
+#While other string literals always have a constant value, formatted strings are really expressions evaluated at run time.
+#It is used to make the code more simpler and easy to understand
+
 print(f"This is my product: {product}")
